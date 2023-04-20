a9dbd488d0af896c80ea865bf8256baf5e24cc38
Mohi <MOHISADEGHI302@GMAIL.com>
2023-04-20 12:41:56 +0330

    Add method fizzbuzz

--- solver.rb
+++ solver.rb
@@ -3,5 +3,15 @@ class Solver
     raise ArgumentError, "Number must be postitve" if n < 0
     n == 0 ? 1 : n * factorial(n-1)
   end
-
+ 
+  def fizzbuzz(n)
+    if n % 5 == 0
+      "buzz"
+    elsif n % 3 == 0
+      "fizz"
+    elsif n % 5 == 0 && n % 3 == 0
+      "fizzbuzz"
+    else
+      n
+    end
 end
